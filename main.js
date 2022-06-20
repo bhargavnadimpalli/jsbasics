2e94ae6021c4cafbcc784970a70d45c7a11bc3a9
bhargav27b <bhargav@sketchbrahma.com>
2022-06-20 21:45:49 +0530

    ternary operator or short hand for ifelse condition

--- main.js
+++ main.js
@@ -1,27 +1,5 @@
-const toDos = [
-    {
-        no: 1,
-        task: 'workout',
-        isCompleted: true
-    },
+const age = 23;
 
-    {
-        no: 2,
-        task: 'breakfast',
-        isCompleted: false
-    },
+const eligible = age < 30 && age > 18 ? 'eligible' : 'not eligible';
 
-    {
-        no: 3,
-        task: 'go to office',
-        isCompleted: true
-    }
-];
-
-const completedTasks = toDos.filter(function(completedTasksList){
-    return completedTasksList.isCompleted === true;
-}).map(function(completedTasksNames){
-    return completedTasksNames.task;
-});
-
-console.log(completedTasks);
+console.log(eligible);
