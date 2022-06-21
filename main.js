bd26efa8dc61cf81dd515ec863bd74a5e6fa6e5a
bhargav27b <bhargav@sketchbrahma.com>
2022-06-21 19:59:41 +0530

    switch conditions

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ const age = 23;
 
 const eligible = age < 30 && age > 18 ? 'eligible' : 'not eligible';
 
-console.log(eligible);
+switch(eligible){
+    case 'eligible':
+        console.log('you are eligible');
+        break;
+
+    case 'not eligible':
+        console.log('you are not eligible');
+        break;
+
+}
