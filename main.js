7ce14780caaae7f7bce95dc8bf9b8d56def36f2b
bhargav27b <bhargav@sketchbrahma.com>
2022-06-21 22:47:22 +0530

    prototypes, for moving the methods in the constructor into prototypes. Useful to avoid returning the methods by default

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ function Person(firstName, LastName, dob){
     this.firstName = firstName;
     this.LastName = LastName;
     this.dob = new Date(dob);
-    this.birthYear = () => this.dob.getFullYear();
-    this.fullName = () => `${firstName} ${LastName}`;
+}
+
+Person.prototype.birthYear = function() {
+    return this.dob.getFullYear();
+}
+
+Person.prototype.fullName = function() {
+    return `${this.firstName} ${this.LastName}`;
 }
 
 //Instantiate Object
 const person1 = new Person('nadimpalli', 'bhargav', '11-22-1998');
 
-console.log(person1.birthYear());
 console.log(person1.fullName());
+console.log(person1);
