7ea8b5c35e9ee49028b2fe3470d1e239c2c6ab0e
bhargav27b <bhargav@sketchbrahma.com>
2022-06-21 22:56:26 +0530

    class constructors. Same as previous commit. that is functional constructor, but with less and structured code. This is the es6 feature

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-//constructor function
-function Person(firstName, LastName, dob){
-    this.firstName = firstName;
-    this.LastName = LastName;
-    this.dob = new Date(dob);
-}
+//class constructors
+class Person {
+    constructor(firstName, LastName, dob){
+        this.firstName = firstName;
+        this.LastName = LastName;
+        this.dob = new Date(dob);
+    }
 
-Person.prototype.birthYear = function() {
-    return this.dob.getFullYear();
-}
+    birthYear() {
+        return this.dob.getFullYear();
+    }
+    
+    fullName() {
+        return `${this.firstName} ${this.LastName}`;
+    }
 
-Person.prototype.fullName = function() {
-    return `${this.firstName} ${this.LastName}`;
 }
 
 //Instantiate Object
