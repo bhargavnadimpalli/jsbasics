9329195b9ca34be13ba3d30be3b27a71c6b964b3
bhargav27b <bhargav@sketchbrahma.com>
2022-06-21 20:03:24 +0530

    function with parameters

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-const age = 31;
-
-const eligible = 'not selected';
-
-switch(eligible){
-    case 'eligible':
-        console.log('you are eligible');
-        break;
-
-    case 'not eligible':
-        console.log('you are not eligible');
-        break;
-
-    default:
-        console.log('please select the age');
-        break;
-
+function addition(firstNum, secondNum){
+    console.log(firstNum + secondNum);
 }
+
+addition(2, 2);
