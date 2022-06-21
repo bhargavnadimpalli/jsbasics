40f99533aa1f59e5cdaf153767fb883aab4b9f64
bhargav27b <bhargav@sketchbrahma.com>
2022-06-21 22:29:03 +0530

    Instantiate the constructor

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function Person(firstName, LastName, dob){
     this.LastName = LastName;
     this.dob = dob;
 }
+
+//Instantiate Object
+const person1 = new Person('nadimpalli', 'bhargav', '22-11-1998');
+
+console.log(person1);
