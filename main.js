9875e68e2ed4b614e583e0f62595c64979ab817a
bhargav27b <bhargav@sketchbrahma.com>
2022-06-22 21:45:50 +0530

    DOM selector

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-//class constructors
-class Person {
-    constructor(firstName, LastName, dob){
-        this.firstName = firstName;
-        this.LastName = LastName;
-        this.dob = new Date(dob);
-    }
+const form = document.getElementById('my-form');
 
-    birthYear() {
-        return this.dob.getFullYear();
-    }
-    
-    fullName() {
-        return `${this.firstName} ${this.LastName}`;
-    }
-
-}
-
-//Instantiate Object
-const person1 = new Person('nadimpalli', 'bhargav', '11-22-1998');
-
-console.log(person1.fullName());
-console.log(person1);
+console.log(form);
